6d9238d8041b17da14f17f39be01ee938b01026f
Rikuya Fujimoto <mafron@192.168.0.9>
2024-08-15 03:18:01 +0900

    add test for DiskManager

--- src/disk_manager.rs
+++ src/disk_manager.rs
@@ -3,7 +3,7 @@ use std::path::Path;
 use std::io::{self, prelude::*, SeekFrom};
 
 // ページサイズ：4096Byte固定
-const PAGE_SIZE: u64 = 4096;
+const PAGE_SIZE: usize = 4096;
 
 // ディスクマネージャ
 // ・ディスクへのファイル（ヒープファイル）の読み書きを行う
@@ -18,6 +18,7 @@ pub struct DiskManager {
 }
 
 // ページID（NewTypeパターン）
+#[derive(Clone, Copy, Debug)]
 pub struct PageID(pub u64);
 
 impl PageID {
@@ -38,7 +39,7 @@ impl DiskManager {
 
         // ファイルサイズの取得から次に採番するページIDを計算 
         let file_size = heap_file.metadata()?.len();
-        let next_page_id = file_size / PAGE_SIZE;
+        let next_page_id = file_size / PAGE_SIZE as u64;
 
         Ok(Self {
             heap_file,
@@ -56,7 +57,7 @@ impl DiskManager {
     // データの読み込み
     pub fn read(&mut self, page_id: PageID, data: &mut [u8]) -> io::Result<()> {
         // ファイルディスクリプタを読み込むデータの先頭にシーク
-        let offset = PAGE_SIZE * page_id.to_u64();
+        let offset = PAGE_SIZE as u64 * page_id.to_u64();
         self.heap_file.seek(SeekFrom::Start(offset))?;
 
         // データの読み込み
@@ -65,10 +66,51 @@ impl DiskManager {
 
     // データの書き込み
     pub fn write(&mut self, page_id: PageID, data: &[u8]) -> io::Result<()> {
-        let offset = PAGE_SIZE * page_id.to_u64();
+        let offset = PAGE_SIZE as u64 * page_id.to_u64();
         self.heap_file.seek(SeekFrom::Start(offset))?;
 
         // データの書き込み
         self.heap_file.write_all(data)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tempfile::NamedTempFile;
+
+    #[test]
+    fn test() {
+        let (_, data_file_path) = NamedTempFile::new().unwrap().into_parts();
+        let mut disk = DiskManager::new(&data_file_path).unwrap();
+
+        // PageIDの採番
+        let test_page_id = disk.allocate_page();
+
+        // tempファイルへの書き込み
+        let mut data = Vec::with_capacity(PAGE_SIZE); // ページサイズのベクターを確保
+        data.extend_from_slice(b"test"); // 文字列の格納
+        data.resize(PAGE_SIZE, 0); // 0-padding
+
+        disk.write(test_page_id, &data).unwrap();
+
+        let second_test_page_id = disk.allocate_page();
+        let mut data2 = Vec::with_capacity(PAGE_SIZE);
+        data2.extend_from_slice(b"second_test");
+        data2.resize(PAGE_SIZE, 0);
+
+        disk.write(second_test_page_id, &data2).unwrap();
+
+        drop(disk);
+
+        // tempファイルの読み込み
+        let mut disk2 = DiskManager::new(data_file_path).unwrap();
+
+        let mut buffer = vec![0; PAGE_SIZE];
+        disk2.read(test_page_id, &mut buffer);
+        assert_eq!(data, buffer);
+        disk2.read(second_test_page_id, &mut buffer);
+        assert_eq!(data2, buffer);
     }
 }
